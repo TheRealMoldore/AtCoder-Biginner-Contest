85042677a1d001d1e33419d94583ff8961fe9f23
KL-Lru <kilattoeruru@outlook.jp>
2017-03-15 04:41:50 +0900

    Add files via upload

---
+++ ABC54A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int A,B;
+    cin>>A>>B;
+    if(A==1)A=14;
+    if(B==1)B=14;
+    if(A>B)cout<<"Alice"<<endl;
+    if(B>A)cout<<"Bob"<<endl;
+    if(A==B)cout<<"Draw"<<endl;
+}
